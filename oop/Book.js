76dc1574483ef4949beb85269c44c2e16870e623
Iryna Suhak <irynasuhak9@gmail.com>
2024-11-26 11:53:08 +0200

    move abjects and checks into task1.js

--- oop/Book.js
+++ oop/Book.js
@@ -15,11 +15,3 @@ class Book {
         console.log(`${this.title} by ${this.author}, published in ${this.year}.`);
     }
 }
-
-const book1 = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 1925);
-const book2 = new Book('To Kill a Mockingbird', 'Harper Lee', 1960);
-const book3 = new Book('Pride and Prejudice', 'Jane Austen', 1813);
-
-book1.printInfo();
-book2.printInfo();
-book3.printInfo();
